938d9d84aebaa6dfa84abe2f0cb8128db13cf90a
EagleCoder404 <harsha.jediknight@gmail.com>
2019-11-09 16:37:38 +0530

    x

---
+++ login/logingit.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<title>::LOGIN::</title>
+<link href="logingit.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+<div class="logo">
+<h2>GitHub</h2>
+</div>
+<div class="nav">
+<ul>
+<li><a>DEVELOPER</a></li>
+<li><a>CONTACT US</a></li>
+</ul>
+</div>
+<div class="right">
+<img src="icon1.png" />
+<div class="form">
+<form  method="post">
+
+<table>
+<tr class="label"><center><strong><h1>LOGIN</h1></strong></center></tr>
+<tr><td><input type="text" placeholder="Username or email address" name="user" size="30" required></td></tr>
+<tr><td><input type="password" placeholder="Password" name="pass" size="30" required></td></tr>
+<tr><td><p><a href="#">Forget password?</a></p></td></tr>
+<tr><td><button type="submit" name="submit" class="submit">LOGIN</button></td></tr>
+
+</table>
+</form>
+</div>
+<div class="account">
+	<p>I'm new..</p>
+	<button href="#" name="create" class="create">CREATE AN ACCOUNT</button>
+</div>
+</div>
+</div>
+</body>
+</html>
